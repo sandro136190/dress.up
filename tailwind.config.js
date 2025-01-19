e8480f63560f515f2b3679a61541972d977ab5fa
sandro136190 <sa.bochorishvili@gmail.com>
2025-01-19 15:46:39 +0400

    background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "custom-bg":
-          "url('https://scontent.xx.fbcdn.net/v/t1.15752-9/431346604_3613763545507188_2878860993630188080_n.png?stp=dst-png_s720x720&_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=FsGwS-2TApcQ7kNvgHk6-Pn&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1QEt3gil4TaUXOSjXzzgdGUfvg77AzCze2awX6pz6zZWTg&oe=677D02A8')",
+          "url(https://html.design/demo/eflyer/images/banner-bg.png)",
       },
       colors: {
         background: "var(--background)",
